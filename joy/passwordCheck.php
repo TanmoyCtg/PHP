7b6c91fdf0bb359d8482713c7ccd620f11637907
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2018-07-08 01:52:41 +0600

    password logic

---
+++ joy/passwordCheck.php
@@ -0,0 +1,48 @@
+<?php
+$error = '';
+$confirmMsg = '';
+if (filter_has_var(INPUT_POST, 'submit')){
+	$password = $_POST['password'];
+	$confirmPassword = $_POST['confirmPassword'];
+
+	if (empty($password) || empty($confirmPassword)){
+		$error = "enter password";
+	}
+	else{
+		if (($password) == ($confirmPassword)){
+			if (strlen($password) > 6 && strlen($confirmPassword) > 6  ){ 
+				$confirmMsg = "thank you! it's ok!";
+			} else{
+				$error = "use char and number together.at least 7 digit";
+			}	
+		}	
+		else{
+			$error = "password don't match";
+		}
+	}
+
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password</title>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+	<?php
+		if ($error == true){echo $error."<br>";}
+		else{ echo $confirmMsg."<br>";}
+		
+	?>
+	<label>password</label>
+	<input type="password" name="password" ><br>
+	<label>confirm password</label>
+	<input type="password" name="confirmPassword">
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
