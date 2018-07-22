968db80dba9058840655a80da92c861b7ba2346e
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2018-07-22 22:01:36 +0600

    date function

---
+++ php-intro/code/arrays/date.php
@@ -0,0 +1,8 @@
+<?php
+  echo "Today is ".date("Y/m/d")."<br>";
+  echo "Today is ".date("Y.m.d")."<br>";
+  echo "Today is ".date("Y-m-d")."<br>";
+  echo "Today is ".date("d-m-Y")."<br>";
+  echo "Today is ". date("l")."<br>";
+  echo "copyright &copyiftekharjoy ". date("d/m/Y");
+?>
