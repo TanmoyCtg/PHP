7b36b1542fb62f8af9af775223ff19bb2120223f
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2018-07-23 15:32:04 +0600

    updated
    Like, insert into, select * means all columns

---
+++ SQL/exercise.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE People DEFAULT CHARACTER SET utf8 
+CREATE TABLE users( name varchar(30), email varchar(30) )
+
+INSERT INTO users(name, email) VALUES ("Chuck","csev@umich.edu");
+INSERT INTO users(name, email) VALUES ("caitlin","cat@umich.edu") ;
+INSERT INTO users(name, email) VALUES ("sally","sally@umich.edu") ;
+INSERT INTO users(name, email) VALUES ("somesh","somesh@umich.edu") ;
+INSERT INTO users(name, email) VALUES ("Ted","ted@umich.edu") ;
+
+DELETE FROM users WHERE email = "ted@umich.edu" 
+UPDATE users SET name="charles" WHERE email="csev@umich.edu"
+
+//Like is a wildcard clause. it's use for searching. search for matching.
+SELECT * FROM `users` WHERE name LIKE '%e%'
+//show all the people who have "e" in their name
+
+SELECT * FROM `users` WHERE 1
