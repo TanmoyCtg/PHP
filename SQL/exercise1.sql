fb8d5130538c68e01ce89c284cd566ba8990f9d8
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2018-07-26 16:19:17 +0600

    updated

--- SQL/exercise1.sql
+++ SQL/exercise1.sql
@@ -64,8 +64,65 @@ INSERT INTO Track (title, rating, len, count, album_id, genre_id)
 INSERT INTO Track (title, rating, len, count, album_id, genre_id)
     VALUES ('Who Made Who', 5, 207, 0, 1, 2);
      
+------------------------------------ Assignment-----------------------------------------
+
+INSERT INTO `artist`(`artist_id`, `name`) VALUES (3,"joy") 
+INSERT INTO `album`(`album_id`, `title`, `artist_id`) VALUES (4,"UP AND UP",1);
+INSERT INTO `album`(`album_id`, `title`, `artist_id`) VALUES (5,"NIGGA NIGGA",2);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (5,"ROCK AND ROLL",300,4,0,3,1);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (6,"life without you",200,5,0,4,2);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (7,"UP AND UP",240,5,0,2,1);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (8,"YO NIGGI",350,3,0,5,2);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (9,"Girls Like You ft cardi B",300,4,0,1,1);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (10,"Cheap Thrills ft sean paul",400,4,0,4,2);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (11,"Fix you",340,5,0,5,1);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (12,"Simple Plan",310,3,0,2,2);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (13,"Perfectly Perfect",350,5,0,1,1);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (14,"Photograph",324,3,0,5,2);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (15,"Galway Girl",280,4,0,4,1);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (16,"Despacito",330,5,0,3,2);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (17,"Back to you",300,5,0,2,1);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (18,"Something just like this",300,4,0,1,2);
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (19,"old friends",300,4,0,2,2);
+
+
+INSERT INTO `track`(`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES (20,"Faded",300,4,0,1,1);
+
+
+SELECT album.title, track.title,artist.name,genre.name FROM album JOIN artist JOIN genre JOIN track ON 
+	album.album_id = track.album_id AND 
+	genre.genre_id = track.genre_id AND 
+    album.artist_id = artist.artist_id ORDER BY
+    album.title ASC;
     
     
+SELECT DISTINCT artist.name,genre.name FROM track JOIN album JOIN genre JOIN artist ON 
+	track.album_id = album.album_id AND 
+	genre.genre_id = track.genre_id AND 
+	artist.artist_id = album.artist_id
+	WHERE artist.name='Led Zepplin'
+
     
     
 
